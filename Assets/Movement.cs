7b3b6a9ae778a97501bd772cc2a0d54f557cb273
Joe Greive <swirllyman@gmail.com>
2016-03-19 14:24:45 -0700

    I believe I can fly!

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+    //[Tooltip("Value for character speed.")]
     public float speed = 24;
+    public float elevationSpeed = 10;
     Transform cam;
 
     void Start() {
@@ -13,10 +15,20 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         cam.position = transform.position;
-        transform.forward = Camera.main.transform.forward;
+        transform.forward = cam.forward;
+
         //increments the object based on the Vector 3 that is passed in
-        transform.Translate((Input.GetAxisRaw("Horizontal") * Time.deltaTime) * speed, 
-            0, 
-            (Input.GetAxisRaw("Vertical") * Time.deltaTime) * speed);
+        transform.Translate((Input.GetAxisRaw("Horizontal") * Time.deltaTime) * speed, 0, (Input.GetAxisRaw("Vertical") * Time.deltaTime) * speed);
+
+        //GetKeyDwn() ---- WRONG
+        //GetKey() ----    RIGHT
+
+        if (Input.GetKey(KeyCode.Q)) {
+            transform.position += transform.up * Time.deltaTime * elevationSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.E)) {
+            transform.position -= transform.up * Time.deltaTime * elevationSpeed;
+        }
 	}
 }
