4a9ba7aa013a412b4117191e3f5e2db6b8f917c5
Joe Greive <swirllyman@gmail.com>
2016-03-19 13:20:51 -0700

    initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 24;
+    Transform cam;
+
+    void Start() {
+        cam = Camera.main.transform.parent;
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        cam.position = transform.position;
+        transform.forward = Camera.main.transform.forward;
+        //increments the object based on the Vector 3 that is passed in
+        transform.Translate((Input.GetAxisRaw("Horizontal") * Time.deltaTime) * speed, 
+            0, 
+            (Input.GetAxisRaw("Vertical") * Time.deltaTime) * speed);
+	}
+}
